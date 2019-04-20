1f50f591018f2965d372a97745cff5b4488d1f25
lixiang <csucoderlee@foxmail.com>
2019-04-20 23:18:02 +0800

    init app.java

---
+++ src/main/java/org/csu/App.java
@@ -0,0 +1,10 @@
+package org.csu;
+
+/**
+ * Created by csucoderlee on 2019/4/20
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
